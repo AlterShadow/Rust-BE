ea6555778f67f154b3dd8752637c5a403dfdc96e
qiujiangkun <qjk2001@gmail.com>
2023-05-13 00:37:16 +0800

    [dev] update endpoints.rs WIP

--- src/service/user/endpoints.rs
+++ src/service/user/endpoints.rs
@@ -1,43 +1,86 @@
 use model::endpoint::*;
 use model::types::{Field, Type};
 
-pub fn endpoint_user_transfer_organization_owner() -> EndpointSchema {
-    EndpointSchema::new(
-        "TransferOrganizationOwner",
-        20040,
-        vec![
-            Field::new("organization_id", Type::BigInt),
-            Field::new("transfer_organization_owner_key", Type::String),
-            Field::new("new_owner_user_id", Type::BigInt),
-        ],
-        vec![],
-    )
+pub fn endpoint_user_follow_strategy() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_list_followed_strategy() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_unfollow_strategy() -> EndpointSchema {
+    todo!()
 }
 
-pub fn endpoint_user_list_organization_membership() -> EndpointSchema {
-    EndpointSchema::new(
-        "ListOrganizationMembership",
-        20042,
-        vec![],
-        vec![Field::new(
-            "memberships",
-            Type::data_table(
-                "ListOrganizationMembershipRow",
-                vec![
-                    Field::new("organization_id", Type::BigInt),
-                    Field::new("organization_name", Type::String),
-                    Field::new("user_id", Type::BigInt),
-                    Field::new("role", Type::enum_ref("role")),
-                    Field::new("accepted", Type::Boolean),
-                ],
-            ),
-        )],
-    )
+pub fn endpoint_user_list_strategy() -> EndpointSchema {
+    todo!()
 }
 
+pub fn endpoint_user_get_strategy() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_get_strategy_statistics() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_back_strategy() -> EndpointSchema {
+    todo!()
+}
+
+pub fn endpoint_user_list_backed_strategy() -> EndpointSchema {
+    todo!()
+}
+
+pub fn endpoint_user_list_back_strategy_history() -> EndpointSchema {
+    todo!()
+}
+
+pub fn endpoint_user_exit_strategy() -> EndpointSchema {
+    todo!()
+}
+
+pub fn endpoint_user_list_exit_strategy_history() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_follow_expert() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_list_followed_expert() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_unfollow_expert() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_list_experts() -> EndpointSchema {
+    todo!()
+}
+
+pub fn endpoint_user_get_expert_profile() -> EndpointSchema {
+    todo!()
+}
+
+pub fn endpoint_user_get_user_profile() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_register_wallet() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_list_wallets() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_deregister_wallet() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_apply_become_expert() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_admin_approve_user_become_expert() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_make_strategy() -> EndpointSchema {
+    todo!()
+}
+pub fn endpoint_user_update_strategy() -> EndpointSchema {
+    todo!()
+}
 pub fn get_user_endpoints() -> Vec<EndpointSchema> {
-    vec![
-        endpoint_user_transfer_organization_owner(),
-        endpoint_user_list_organization_membership(),
-    ]
+    vec![todo!()]
 }
