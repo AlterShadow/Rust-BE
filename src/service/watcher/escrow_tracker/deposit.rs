dbd46e425eeb6dd0405ac02682d99369440b86f5
sergera <sergera88@gmail.com>
2023-05-24 20:43:55 -0300

    [test] mock erc 20 bytecode, abi, wrapper, and tests

--- src/service/watcher/escrow_tracker/deposit.rs
+++ src/service/watcher/escrow_tracker/deposit.rs
@@ -210,13 +210,274 @@ mod tests {
     use lib::log::{setup_logs, LogLevel};
     use secp256k1::SecretKey;
     use std::str::FromStr;
+    use std::thread::sleep;
+    use std::time::Duration;
     use tracing::info;
+    use web3::contract::{Contract, Options};
     use web3::signing::Key;
-    const ANVIL_PRIV_KEY: &str = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    use web3::types::{TransactionReceipt, H256, U64};
+    use web3::Transport;
+    const ANVIL_PRIV_KEY_1: &str =
+        "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    const ANVIL_PRIV_KEY_2: &str =
+        "59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
+    const ANVIL_PRIV_KEY_3: &str =
+        "5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a";
+    const ANVIL_PRIV_KEY_4: &str =
+        "7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6";
+
+    struct MockERC20Contract<T: Transport> {
+        /* unrestricted mint and burn, but all other restrictions apply */
+        pub inner: Contract<T>,
+    }
+
+    impl<T: Transport> MockERC20Contract<T> {
+        pub fn new(contract: Contract<T>) -> Result<Self> {
+            Ok(Self { inner: contract })
+        }
+
+        pub async fn mint(&self, secret: impl Key, to: Address, amount: U256) -> Result<H256> {
+            Ok(self
+                .inner
+                .signed_call("mint", (to, amount), Options::default(), secret)
+                .await?)
+        }
+
+        pub async fn burn(&self, secret: impl Key, from: Address, amount: U256) -> Result<H256> {
+            Ok(self
+                .inner
+                .signed_call("burn", (from, amount), Options::default(), secret)
+                .await?)
+        }
+
+        pub async fn transfer(&self, secret: impl Key, to: Address, amount: U256) -> Result<H256> {
+            Ok(self
+                .inner
+                .signed_call("transfer", (to, amount), Options::default(), secret)
+                .await?)
+        }
+
+        pub async fn transfer_from(
+            &self,
+            secret: impl Key,
+            from: Address,
+            to: Address,
+            amount: U256,
+        ) -> Result<H256> {
+            Ok(self
+                .inner
+                .signed_call(
+                    "transferFrom",
+                    (from, to, amount),
+                    Options::default(),
+                    secret,
+                )
+                .await?)
+        }
+
+        pub async fn approve(
+            &self,
+            secret: impl Key,
+            spender: Address,
+            amount: U256,
+        ) -> Result<H256> {
+            Ok(self
+                .inner
+                .signed_call("approve", (spender, amount), Options::default(), secret)
+                .await?)
+        }
+
+        pub async fn balance_of(&self, owner: Address) -> Result<U256> {
+            Ok(self
+                .inner
+                .query("balanceOf", owner, None, Options::default(), None)
+                .await?)
+        }
+
+        pub async fn allowance(&self, owner: Address, spender: Address) -> Result<U256> {
+            Ok(self
+                .inner
+                .query(
+                    "allowance",
+                    (owner, spender),
+                    None,
+                    Options::default(),
+                    None,
+                )
+                .await?)
+        }
+
+        pub async fn total_supply(&self) -> Result<U256> {
+            Ok(self
+                .inner
+                .query("totalSupply", (), None, Options::default(), None)
+                .await?)
+        }
+    }
+
+    async fn deploy_mock_erc20<T: Transport>(
+        conn: Eth<T>,
+        key: impl Key,
+    ) -> Result<MockERC20Contract<T>> {
+        let bytecode = include_str!("mock_erc20.bin");
+        let abi_file = File::open("abi/test/mock_erc20.json")?;
+        let reader = BufReader::new(abi_file);
+        let abi_json: serde_json::Value = serde_json::from_reader(reader)?;
+        let deployer = ContractDeployer::new(conn, abi_json)?.code(bytecode.to_owned());
+        Ok(MockERC20Contract::new(
+            deployer.sign_with_key_and_execute((), key).await?,
+        )?)
+    }
+
+    #[derive(Debug, PartialEq)]
+    enum TxStatus {
+        Successful,
+        Reverted,
+        Unknown,
+    }
+
+    struct TxChecker<T: Transport> {
+        conn: Eth<T>,
+    }
+
+    impl<T: Transport> TxChecker<T> {
+        fn new(conn: Eth<T>) -> Self {
+            Self { conn }
+        }
+
+        async fn status(&self, tx_hash: H256) -> Result<TxStatus> {
+            /* typically 1 millisecond is enough to guarantee an Anvil receipt */
+            sleep(Duration::from_millis(5));
+            match self.conn.transaction_receipt(tx_hash).await? {
+                Some(receipt) => {
+                    if receipt.status == Some(web3::types::U64::from(1)) {
+                        Ok(TxStatus::Successful)
+                    } else {
+                        Ok(TxStatus::Reverted)
+                    }
+                }
+                None => Ok(TxStatus::Unknown),
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_mock_erc20_contract() -> Result<()> {
+        let key = Secp256k1SecretKey::from_str(ANVIL_PRIV_KEY_1)?;
+        let conn_pool = EthereumRpcConnectionPool::localnet();
+        let conn = conn_pool.get_conn().await?;
+        let mock_erc20 = deploy_mock_erc20(conn.into_raw().eth(), &key).await?;
+        let tx_checker = TxChecker::new(conn_pool.get_conn().await?.into_raw().eth());
+
+        let alice = Secp256k1SecretKey::from_str(ANVIL_PRIV_KEY_2)?;
+        let bob = Secp256k1SecretKey::from_str(ANVIL_PRIV_KEY_3)?;
+        let charlie = Secp256k1SecretKey::from_str(ANVIL_PRIV_KEY_4)?;
+
+        /* positive assertions */
+        assert_eq!(mock_erc20.balance_of(alice.address).await?, U256::from(0));
+        assert_eq!(mock_erc20.total_supply().await?, U256::from(0));
+        mock_erc20.mint(&key, alice.address, U256::from(10)).await?;
+        assert_eq!(mock_erc20.balance_of(alice.address).await?, U256::from(10));
+        assert_eq!(mock_erc20.total_supply().await?, U256::from(10));
+        mock_erc20.burn(&key, alice.address, U256::from(5)).await?;
+        assert_eq!(mock_erc20.balance_of(alice.address).await?, U256::from(5));
+        assert_eq!(mock_erc20.total_supply().await?, U256::from(5));
+
+        mock_erc20
+            .transfer(&alice, bob.address, U256::from(5))
+            .await?;
+        assert_eq!(mock_erc20.balance_of(alice.address).await?, U256::from(0));
+        assert_eq!(mock_erc20.balance_of(bob.address).await?, U256::from(5));
+
+        mock_erc20
+            .approve(&bob, charlie.address, U256::from(5))
+            .await?;
+        assert_eq!(mock_erc20.balance_of(bob.address).await?, U256::from(5));
+        assert_eq!(mock_erc20.balance_of(charlie.address).await?, U256::from(0));
+        assert_eq!(
+            mock_erc20.allowance(bob.address, charlie.address).await?,
+            U256::from(5),
+        );
+        assert_eq!(mock_erc20.total_supply().await?, U256::from(5));
+
+        mock_erc20
+            .transfer_from(&charlie, bob.address, alice.address, U256::from(5))
+            .await?;
+        assert_eq!(
+            mock_erc20.allowance(bob.address, charlie.address).await?,
+            U256::from(0),
+        );
+        assert_eq!(mock_erc20.balance_of(alice.address).await?, U256::from(5));
+        assert_eq!(mock_erc20.balance_of(bob.address).await?, U256::from(0));
+        assert_eq!(mock_erc20.balance_of(charlie.address).await?, U256::from(0));
+        assert_eq!(mock_erc20.total_supply().await?, U256::from(5));
+
+        /* reset */
+        mock_erc20.burn(&key, alice.address, U256::from(5)).await?;
+        assert_eq!(mock_erc20.total_supply().await?, U256::from(0));
+
+        /* negative assertions */
+        assert_eq!(
+            tx_checker
+                .status(
+                    mock_erc20
+                        .transfer(&alice, bob.address, U256::from(1))
+                        .await?
+                )
+                .await?,
+            TxStatus::Reverted,
+        );
+        assert_eq!(
+            tx_checker
+                .status(
+                    mock_erc20
+                        .transfer(&bob, alice.address, U256::from(1))
+                        .await?
+                )
+                .await?,
+            TxStatus::Reverted,
+        );
+        assert_eq!(
+            tx_checker
+                .status(
+                    mock_erc20
+                        .transfer(&charlie, alice.address, U256::from(1))
+                        .await?
+                )
+                .await?,
+            TxStatus::Reverted,
+        );
+        mock_erc20.mint(&key, alice.address, U256::from(10)).await?;
+        mock_erc20
+            .approve(&alice, bob.address, U256::from(5))
+            .await?;
+        assert_eq!(
+            tx_checker
+                .status(
+                    mock_erc20
+                        .transferFrom(&bob, alice.address, charlie.address, U256::from(6),)
+                        .await?
+                )
+                .await?,
+            TxStatus::Reverted,
+        );
+        assert_eq!(
+            tx_checker
+                .status(
+                    mock_erc20
+                        .transfer(&alice, charlie.address, U256::from(11))
+                        .await?
+                )
+                .await?,
+            TxStatus::Reverted,
+        );
+        Ok(())
+    }
+
     #[tokio::test]
     async fn test_user_ethereum_testnet_transfer() -> Result<()> {
         let _ = setup_logs(LogLevel::Trace);
-        let key = Secp256k1SecretKey::from_str(ANVIL_PRIV_KEY)?;
+        let key = Secp256k1SecretKey::from_str(ANVIL_PRIV_KEY_1)?;
         let conn_pool = EthereumRpcConnectionPool::localnet();
         let conn = conn_pool.get_conn().await?;
         let airdrop_tx = conn
@@ -228,7 +489,7 @@ mod tests {
     #[tokio::test]
     async fn test_user_ethereum_deposit() -> Result<()> {
         let _ = setup_logs(LogLevel::Trace);
-        let user_key = Secp256k1SecretKey::from_str(ANVIL_PRIV_KEY)?;
+        let user_key = Secp256k1SecretKey::from_str(ANVIL_PRIV_KEY_1)?;
         let escrow_key = Secp256k1SecretKey::new_random();
         let conn_pool = EthereumRpcConnectionPool::localnet();
         let conn = conn_pool.get_conn().await?;
