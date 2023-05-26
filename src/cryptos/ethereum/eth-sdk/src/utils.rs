6623a2c7f84cc853cb02872477f9e707b15a0194
sergera <sergera88@gmail.com>
2023-05-26 00:06:55 -0300

    [fix] remove ethabi conversion utils

--- src/cryptos/ethereum/eth-sdk/src/utils.rs
+++ src/cryptos/ethereum/eth-sdk/src/utils.rs
@@ -74,19 +74,6 @@ pub fn encode_signature(sig: &Signature) -> String {
     sig_bytes.push(sig.v as u8 + 27);
     hex::encode(sig_bytes)
 }
-pub fn convert_h160_ethabi_to_web3(ethabi_h160: ethabi::ethereum_types::H160) -> H160 {
-    H160::from_slice(&ethabi_h160.0)
-}
-
-pub fn convert_h256_ethabi_to_web3(ethabi_h256: ethabi::ethereum_types::H256) -> H256 {
-    H256::from_slice(&ethabi_h256.0)
-}
-
-pub fn convert_u256_ethabi_to_web3(ethabi_u256: ethabi::ethereum_types::U256) -> U256 {
-    let mut buffer = [0u8; 32];
-    ethabi_u256.to_big_endian(&mut buffer);
-    U256::from_big_endian(&buffer)
-}
 
 #[cfg(test)]
 mod tests {
