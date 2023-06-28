1903aa8ab956919fb71d4847e5810a2d358a4e82
qiujiangkun <qjk2001@gmail.com>
2023-06-28 13:14:37 +0800

    [dev] enhance BAD_REQUEST error message

--- src/lib/handler.rs
+++ src/lib/handler.rs
@@ -24,15 +24,24 @@ pub trait RequestHandlerErased: Send + Sync {
 
 impl<T: RequestHandler> RequestHandlerErased for T {
     fn handle(&self, toolbox: &Toolbox, ctx: RequestContext, req: Value) -> BoxFuture<'static, ()> {
+        // TODO: find a better way to avoid double parsing or serialization
+        let buf = serde_json::to_string(&req).unwrap();
         let data: T::Request = match serde_json::from_value(req) {
             Ok(data) => data,
             Err(err) => {
+                let jd = &mut serde_json::Deserializer::from_str(&buf);
+                let data: Result<T::Request, _> = serde_path_to_error::deserialize(jd);
+                let path = data.err().map(|err| err.path().to_string());
                 toolbox.send(
                     ctx.connection_id,
                     request_error_to_resp(
                         &ctx,
                         ErrorCode::new(100400), // Bad Request
-                        err.to_string(),
+                        if let Some(path) = path {
+                            format!("{}: {}", path, err)
+                        } else {
+                            format!("{}", err)
+                        },
                     ),
                 );
                 return async { () }.boxed();
