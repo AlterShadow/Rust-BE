54b021d49f56c234c3c7cbd5a093287e38ed2dbb
sergera <sergera88@gmail.com>
2023-07-06 14:22:01 -0300

    [dev] new deployment addresses for strategy pool herald contract

--- src/service/shared/eth-sdk/src/strategy_pool_herald_addresses.rs
+++ src/service/shared/eth-sdk/src/strategy_pool_herald_addresses.rs
@@ -17,22 +17,22 @@ impl StrategyPoolHeraldAddresses {
         this.insert(
             EnumBlockChain::EthereumMainnet,
             (),
-            Address::from_str("0x8C47839e82243cF5E2EE784B115F68e95f3C2ce1").unwrap(),
+            Address::from_str("0x2096ccFc2EfE5dF7Cc838cF39aa5528891666e51").unwrap(),
         );
         this.insert(
             EnumBlockChain::EthereumGoerli,
             (),
-            Address::from_str("0x14837279c5FC572B0175e078732fb0694287bf53").unwrap(),
+            Address::from_str("0x64c1241aE01b245FBA90BA88e15DF78Da2a6a2D9").unwrap(),
         );
         this.insert(
             EnumBlockChain::BscMainnet,
             (),
-            Address::from_str("0x52f47C22F0138f8c6251b6A4dD6E93ee693116e1").unwrap(),
+            Address::from_str("0x115932B4D979E3d7b2b18066Af444663E7F25478").unwrap(),
         );
         this.insert(
             EnumBlockChain::BscTestnet,
             (),
-            Address::from_str("0x0893abEB433C1a3D63C60F7034c2582Fc7dc8c52").unwrap(),
+            Address::from_str("0x85fC1F9EC12e16DA681EEd853464F9E162e3C036").unwrap(),
         );
 
         Self(this)
