df5d9b171f519daa24586d528a6e2ce89f3d697d
qiujiangkun <qjk2001@gmail.com>
2023-07-07 01:56:53 +0800

    [dev] wording

--- src/service/user/back_strategy.rs
+++ src/service/user/back_strategy.rs
@@ -935,7 +935,7 @@ pub async fn user_back_strategy(
         )
     }
     logger.log(format!(
-        "User backed strategy {:?} with {} USDC. got {} SP tokens",
+        "Everything deployed successfully for Strategy {:?}. Spent {} tokens. got {} SP tokens",
         strategy_id,
         amount_to_display(back_token_amount),
         amount_to_display(strategy_token_to_mint)
