61ded3d02ac878c1b86d13fd73e4b2adaecda25e
qiujiangkun <qjk2001@gmail.com>
2023-07-09 12:35:56 +0800

    [dev] deploy & speed up deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,10 @@ CROSS_TARGET=x86_64-unknown-linux-gnu
 cargo zigbuild --target=$CROSS_TARGET --all --release
 ssh mc2fi 'mkdir -p mc2fi/target/release/ mc2fi/log'
 (cd target/x86_64-unknown-linux-gnu/release/ && rsync -avizh mc2fi_auth mc2fi_user mc2fi_admin mc2fi_watcher mc2fi:mc2fi/target/release/ )
+rsync -avizh scripts db mc2fi:mc2fi/
 scp etc/config.prod.json mc2fi:mc2fi/etc/config.json
 rsync -avizh etc/systemd/*.service root@mc2fi:/etc/systemd/system/
-scripts/remount_functions.sh etc/config.prodref.json
+ssh mc2fi '(cd mc2fi && scripts/remount_functions.sh etc/config.json)'
 ssh root@mc2fi 'bash -s' < scripts/restart_services.sh
 scripts/upload_docs.sh
 
--- scripts/remount_functions.sh
+++ scripts/remount_functions.sh
@@ -2,7 +2,7 @@
 CONFIG=$1
 export PGPASSWORD=$(jq '.app_db.password' -r $CONFIG)
 export PGHOST=$(jq '.app_db.host' -r $CONFIG)
-export PGPORT=$(jq '.app_db.port' -r $CONFIG)
+export PGPORT=$(jq '.app_db.port' -r $CONFIG | sed 's/6432/5432/')
 export PGUSER=$(jq '.app_db.user' -r $CONFIG)
 export PGPASSWORD=$(jq '.app_db.password' -r $CONFIG)
 export PGDATABASE=$(jq '.app_db.dbname' -r $CONFIG)
