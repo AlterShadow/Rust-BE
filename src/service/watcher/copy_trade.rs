53ac2398b6b7808f4e580a10738aa8b6be5a6204
sergera <sergera88@gmail.com>
2023-07-18 11:58:54 -0300

    [feat] fetch strategy pool asset balances I/O function

--- src/service/watcher/copy_trade.rs
+++ src/service/watcher/copy_trade.rs
@@ -1,8 +1,46 @@
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+
 use eyre::*;
 use num::ToPrimitive;
 use web3::types::{Address, H256, U256};
 
+use eth_sdk::evm::DexTrade;
 use eth_sdk::ScaledMath;
+use gen::database::*;
+use gen::model::EnumBlockChain;
+use lib::database::DbClient;
+
+pub async fn fetch_strategy_pool_contract_asset_balances_and_decimals(
+    db: &DbClient,
+    chain: EnumBlockChain,
+    strategy_id: i64,
+) -> Result<(HashMap<Address, U256>, HashMap<Address, usize>)> {
+    let strategy_pool_asset_rows = db
+        .execute(FunWatcherListStrategyPoolContractAssetBalancesReq {
+            strategy_pool_contract_id: None,
+            strategy_id: Some(strategy_id),
+            blockchain: Some(chain),
+            token_address: None,
+        })
+        .await?
+        .into_rows();
+
+    let mut strategy_pool_balances: HashMap<Address, U256> = HashMap::new();
+    let mut strategy_pool_decimals: HashMap<Address, usize> = HashMap::new();
+    for strategy_pool_asset in strategy_pool_asset_rows {
+        strategy_pool_balances.insert(
+            strategy_pool_asset.token_address.into(),
+            strategy_pool_asset.balance.into(),
+        );
+        strategy_pool_decimals.insert(
+            strategy_pool_asset.token_address.into(),
+            strategy_pool_asset.token_decimals.to_usize().unwrap(),
+        );
+    }
+
+    Ok((strategy_pool_balances, strategy_pool_decimals))
+}
 
 pub fn normalize_decimals_to(
     normalize_to: usize,
