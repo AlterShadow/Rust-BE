b5bc6c24cedd8f0f6dd5f4ff3dc616697fe7c7f9
sergera <sergera88@gmail.com>
2023-07-18 11:59:52 -0300

    [feat] fetch listened wallet assets I/O function

--- src/service/watcher/copy_trade.rs
+++ src/service/watcher/copy_trade.rs
@@ -11,6 +11,49 @@ use gen::database::*;
 use gen::model::EnumBlockChain;
 use lib::database::DbClient;
 
+pub async fn fetch_listened_wallet_asset_balances_and_decimals(
+    db: &DbClient,
+    chain: EnumBlockChain,
+    strategy_id: i64,
+) -> Result<(HashMap<Address, U256>, HashMap<Address, usize>)> {
+    // TODO MULTICHAIN: get tokens of watched wallets from all chains
+    let wallet_balance_rows = db
+        .execute(FunWatcherListExpertListenedWalletAssetBalanceReq {
+            limit: None,
+            offset: None,
+            strategy_id: Some(strategy_id),
+            blockchain: Some(chain),
+            address: None,
+            token_id: None,
+        })
+        .await?
+        .into_rows();
+
+    // TODO MULTICHAIN: merge token amounts of the same token to this chain's contract address
+    // TODO MULTICHAIN: normalize token amounts to this chain's contract decimals
+    let mut wallet_balances: HashMap<Address, U256> = HashMap::new();
+    let mut wallet_decimals: HashMap<Address, usize> = HashMap::new();
+    for wallet_asset in wallet_balance_rows {
+        match wallet_balances.entry(wallet_asset.token_address.into()) {
+            Entry::Occupied(mut entry) => {
+                let token_amount = entry.get_mut();
+                *token_amount = token_amount.try_checked_add(wallet_asset.balance.into())?;
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(wallet_asset.balance.into());
+            }
+        }
+        match wallet_decimals.entry(wallet_asset.token_address.into()) {
+            Entry::Vacant(entry) => {
+                entry.insert(wallet_asset.token_decimals.to_usize().unwrap());
+            }
+            _ => continue,
+        }
+    }
+
+    Ok((wallet_balances, wallet_decimals))
+}
+
 pub async fn fetch_strategy_pool_contract_asset_balances_and_decimals(
     db: &DbClient,
     chain: EnumBlockChain,
