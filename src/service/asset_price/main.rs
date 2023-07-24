18685aad8b2b961cdf70dd062b85892726225a6c
sergera <sergera88@gmail.com>
2023-07-24 11:40:48 -0300

    [dev] add config to asset_price service

--- src/service/asset_price/main.rs
+++ src/service/asset_price/main.rs
@@ -1,7 +1,26 @@
 use eyre::*;
+use serde::Deserialize;
+
+use api::cmc::CoinMarketCap;
+use lib::config::load_config;
+use lib::database::{connect_to_database, DatabaseConfig};
+use lib::log::{setup_logs, LogLevel};
+use secrecy::{ExposeSecret, SecretString};
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Config {
+    pub app_db: DatabaseConfig,
+    pub log_level: LogLevel,
+    pub cmc_api_key: SecretString,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let config: Config = load_config("asset_price".to_owned())?;
+    setup_logs(config.log_level)?;
+    let db = connect_to_database(config.app_db).await?;
+    let cmc_client = CoinMarketCap::new(config.cmc_api_key.expose_secret())?;
+
     Ok(())
 }
 
