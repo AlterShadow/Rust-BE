96fc16f9561c750b34a28ee82ed7e8bd6f561f7e
sergera <sergera88@gmail.com>
2023-07-24 15:56:51 -0300

    [dev] auto-formatting

--- src/service/asset_price/pg_func.rs
+++ src/service/asset_price/pg_func.rs
@@ -2,14 +2,15 @@ use model::pg_func::ProceduralFunction;
 use model::types::*;
 
 pub fn get_asset_price_pg_func() -> Vec<ProceduralFunction> {
-    vec![ProceduralFunction::new(
-        "fun_asset_price_insert_asset_prices",
-        vec![
-            Field::new("symbols", Type::Vec(Box::new(Type::String))),
-            Field::new("prices", Type::Vec(Box::new(Type::Numeric))),
-        ],
-        vec![Field::new("success", Type::Boolean)],
-        r#"
+    vec![
+        ProceduralFunction::new(
+            "fun_asset_price_insert_asset_prices",
+            vec![
+                Field::new("symbols", Type::Vec(Box::new(Type::String))),
+                Field::new("prices", Type::Vec(Box::new(Type::Numeric))),
+            ],
+            vec![Field::new("success", Type::Boolean)],
+            r#"
 BEGIN
 	INSERT INTO tbl.token_price (symbol, price, created_at)
 	SELECT a_symbol, a_price, EXTRACT(EPOCH FROM NOW())::bigint
