7a836a42aa9ea3253ed89b2deeef6319add4a2d2
sergera <sergera88@gmail.com>
2023-07-26 12:32:51 -0300

    [feat] add AssetInfoClient trait

--- src/service/shared/api/lib.rs
+++ src/service/shared/api/lib.rs
@@ -1,1 +1,23 @@
 pub mod cmc;
+
+use std::collections::HashMap;
+
+use async_trait::async_trait;
+use eyre::*;
+
+pub struct AssetPriceByPeriod {
+    pub symbol: String,
+    pub price_latest: f64,
+    pub price_1d: Option<f64>,
+    pub price_7d: Option<f64>,
+    pub price_30d: Option<f64>,
+}
+
+#[async_trait]
+pub trait AssetInfoClient {
+    async fn get_usd_price_latest(&self, symbols: &[String]) -> Result<HashMap<String, f64>>;
+    async fn get_usd_price_period(
+        &self,
+        symbols: &[String],
+    ) -> Result<HashMap<String, AssetPriceByPeriod>>;
+}
