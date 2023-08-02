c8f83c141ec2daadc67769935c2047c582695906
sergera <sergera88@gmail.com>
2023-08-02 15:36:38 -0300

    [feat] self revoke adminship endpoints

--- src/service/watcher/main.rs
+++ src/service/watcher/main.rs
@@ -70,10 +70,13 @@ async fn main() -> Result<()> {
         .route("/eth-goerli-withdraws", post(handle_eth_withdraws_goerli))
         .route("/eth-mainnet-redeems", post(handle_eth_redeems_mainnet))
         .route("/eth-goerli-redeems", post(handle_eth_redeems_goerli))
+				.route("/eth-mainnet-revoke-adminships", post(handle_eth_revoke_adminships_mainnet))
+				.route("/eth-goerli-revoke-adminships", post(handle_eth_revoke_adminships_goerli))
         .route("/bsc-mainnet-swaps", post(handle_bsc_swap_mainnet))
         .route("/bsc-mainnet-escrows", post(handle_bsc_escrows_mainnet))
         .route("/bsc-mainnet-withdraws", post(handle_bsc_withdraws_mainnet))
         .route("/bsc-mainnet-redeems", post(handle_bsc_redeems_mainnet))
+				.route("/bsc-mainnet-revoke-adminships", post(handle_bsc_revoke_adminships_mainnet))
         .with_state(Arc::new(
             AppState::new(db, eth_pool, master_key, client, cmc_client, coin_addresses).await?,
         ));
@@ -180,4 +183,25 @@ pub async fn handle_bsc_redeems_mainnet(
     body: Bytes,
 ) -> Result<(), StatusCode> {
     method::handle_eth_redeems(state.0, body, EnumBlockChain::BscMainnet).await
+}
+
+pub async fn handle_eth_revoke_adminships_mainnet(
+		state: State<Arc<AppState>>,
+		body: Bytes,
+) -> Result<(), StatusCode> {
+		method::handle_eth_revoke_adminships(state.0, body, EnumBlockChain::EthereumMainnet).await
+}
+
+pub async fn handle_eth_revoke_adminships_goerli(
+		state: State<Arc<AppState>>,
+		body: Bytes,
+) -> Result<(), StatusCode> {
+		method::handle_eth_revoke_adminships(state.0, body, EnumBlockChain::EthereumGoerli).await
+}
+
+pub async fn handle_bsc_revoke_adminships_mainnet(
+		state: State<Arc<AppState>>,
+		body: Bytes,
+) -> Result<(), StatusCode> {
+		method::handle_eth_revoke_adminships(state.0, body, EnumBlockChain::BscMainnet).await
 }
