00bec03190456db82c3b8d0c5add8852ba656aa9
sergera <sergera88@gmail.com>
2023-08-02 15:47:10 -0300

    [dev] reorder routes to improve readability

--- src/service/watcher/main.rs
+++ src/service/watcher/main.rs
@@ -63,20 +63,29 @@ async fn main() -> Result<()> {
     let coin_addresses = load_coin_addresses(&db).await?;
     let app: Router<(), Body> = Router::new()
         .route("/eth-mainnet-swaps", post(handle_eth_swap_mainnet))
-        .route("/eth-goerli-swaps", post(handle_eth_swap_goerli))
         .route("/eth-mainnet-escrows", post(handle_eth_escrows_mainnet))
-        .route("/eth-goerli-escrows", post(handle_eth_escrows_goerli))
         .route("/eth-mainnet-withdraws", post(handle_eth_withdraws_mainnet))
-        .route("/eth-goerli-withdraws", post(handle_eth_withdraws_goerli))
         .route("/eth-mainnet-redeems", post(handle_eth_redeems_mainnet))
+        .route(
+            "/eth-mainnet-revoke-adminships",
+            post(handle_eth_revoke_adminships_mainnet),
+        )
+        .route("/eth-goerli-swaps", post(handle_eth_swap_goerli))
+        .route("/eth-goerli-escrows", post(handle_eth_escrows_goerli))
+        .route("/eth-goerli-withdraws", post(handle_eth_withdraws_goerli))
         .route("/eth-goerli-redeems", post(handle_eth_redeems_goerli))
-				.route("/eth-mainnet-revoke-adminships", post(handle_eth_revoke_adminships_mainnet))
-				.route("/eth-goerli-revoke-adminships", post(handle_eth_revoke_adminships_goerli))
+        .route(
+            "/eth-goerli-revoke-adminships",
+            post(handle_eth_revoke_adminships_goerli),
+        )
         .route("/bsc-mainnet-swaps", post(handle_bsc_swap_mainnet))
         .route("/bsc-mainnet-escrows", post(handle_bsc_escrows_mainnet))
         .route("/bsc-mainnet-withdraws", post(handle_bsc_withdraws_mainnet))
         .route("/bsc-mainnet-redeems", post(handle_bsc_redeems_mainnet))
-				.route("/bsc-mainnet-revoke-adminships", post(handle_bsc_revoke_adminships_mainnet))
+        .route(
+            "/bsc-mainnet-revoke-adminships",
+            post(handle_bsc_revoke_adminships_mainnet),
+        )
         .with_state(Arc::new(
             AppState::new(db, eth_pool, master_key, client, cmc_client, coin_addresses).await?,
         ));
@@ -186,22 +195,22 @@ pub async fn handle_bsc_redeems_mainnet(
 }
 
 pub async fn handle_eth_revoke_adminships_mainnet(
-		state: State<Arc<AppState>>,
-		body: Bytes,
+    state: State<Arc<AppState>>,
+    body: Bytes,
 ) -> Result<(), StatusCode> {
-		method::handle_eth_revoke_adminships(state.0, body, EnumBlockChain::EthereumMainnet).await
+    method::handle_eth_revoke_adminships(state.0, body, EnumBlockChain::EthereumMainnet).await
 }
 
 pub async fn handle_eth_revoke_adminships_goerli(
-		state: State<Arc<AppState>>,
-		body: Bytes,
+    state: State<Arc<AppState>>,
+    body: Bytes,
 ) -> Result<(), StatusCode> {
-		method::handle_eth_revoke_adminships(state.0, body, EnumBlockChain::EthereumGoerli).await
+    method::handle_eth_revoke_adminships(state.0, body, EnumBlockChain::EthereumGoerli).await
 }
 
 pub async fn handle_bsc_revoke_adminships_mainnet(
-		state: State<Arc<AppState>>,
-		body: Bytes,
+    state: State<Arc<AppState>>,
+    body: Bytes,
 ) -> Result<(), StatusCode> {
-		method::handle_eth_revoke_adminships(state.0, body, EnumBlockChain::BscMainnet).await
+    method::handle_eth_revoke_adminships(state.0, body, EnumBlockChain::BscMainnet).await
 }
