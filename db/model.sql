114dfa224d872856b85eddf9e631b166f9abc528
Eliot128 <domisolamon0921@gmail.com>
2023-09-21 04:33:22 -0400

    Whitelist Registration

--- db/model.sql
+++ db/model.sql
@@ -1,4 +1,4 @@
-CREATE TYPE enum_role AS ENUM ('guest', 'user', 'expert', 'admin', 'developer');
+CREATE TYPE enum_role AS ENUM ('guest', 'user', 'expert', 'admin', 'developer', 'whitelist');
 CREATE TYPE enum_block_chain AS ENUM ('EthereumMainnet', 'EthereumGoerli', 'BscMainnet', 'BscTestnet', 'LocalNet', 'EthereumSepolia');
 CREATE TYPE enum_blockchain_coin AS ENUM ('USDC', 'USDT', 'BUSD', 'WETH', 'WBNB');
 CREATE TYPE enum_dex AS ENUM ('UniSwap', 'PancakeSwap', 'SushiSwap');
